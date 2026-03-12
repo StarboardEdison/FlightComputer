4c28116c34412004d7f9d7b1507ebd933b1338b7
pranavt <72770606+awsame303@users.noreply.github.com>
2026-03-12 16:36:18 -0400

    code (needs some fixes)

---
+++ starboardFlightComputer.ino
@@ -0,0 +1,242 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_BMP3XX.h>
+#include <Servo.h>
+#include <MPU6050_light.h>
+
+#define SERVO_PIN 10
+#define SD_CS_PIN 4
+#define GROUND_PRESSURE_HPA (30.15 * 33.86389)
+
+#define LOOP_DELAY_MS 100
+#define DEPLOY_ALT_MIN_FT 500
+#define TRIGGER_APOGEE_FT 750
+#define PERSIST_COUNT 5
+
+Adafruit_BMP3XX bmp;
+MPU6050 mpu(Wire);
+Servo airbrake;
+
+float groundAlt = 0;
+float altitude_m = 0, velocity_mps = 0, acceleration_mps2 = 0;
+float prev_alt = 0;
+float tilt[3] = {0};
+float accel[3] = {0};
+
+float est_alt = 0, est_vel = 0;
+float P = 1, Q = 0.1, R = 3;
+unsigned long last_time = 0;
+
+bool deployed = false;
+bool retracted = false;
+int above750_count = 0;
+float max_alt = 0;
+bool apogee_logged = false;
+
+char lineBuf[64];
+
+float twoKp = 2.0f * 0.5f;
+float twoKi = 2.0f * 0.0f;
+float integralFBx = 0.0f, integralFBy = 0.0f, integralFBz = 0.0f;
+float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;
+
+float kalmanUpdate(float meas, float &est, float &P) {
+  P += Q;
+  float K = P / (P + R);
+  est += K * (meas - est);
+  P *= (1 - K);
+  return est;
+}
+
+void MahonyAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az, float dt) {
+  float recipNorm;
+  float halfvx, halfvy, halfvz;
+  float halfex, halfey, halfez;
+
+  gx *= 0.0174533f;
+  gy *= 0.0174533f;
+  gz *= 0.0174533f;
+
+  recipNorm = 1.0f / sqrtf(ax * ax + ay * ay + az * az);
+  ax *= recipNorm;
+  ay *= recipNorm;
+  az *= recipNorm;
+
+  halfvx = q1 * q3 - q0 * q2;
+  halfvy = q0 * q1 + q2 * q3;
+  halfvz = q0 * q0 - 0.5f + q3 * q3;
+
+  halfex = (ay * halfvz - az * halfvy);
+  halfey = (az * halfvx - ax * halfvz);
+  halfez = (ax * halfvy - ay * halfvx);
+
+  if (twoKi > 0.0f) {
+    integralFBx += twoKi * halfex * dt;
+    integralFBy += twoKi * halfey * dt;
+    integralFBz += twoKi * halfez * dt;
+    gx += integralFBx;
+    gy += integralFBy;
+    gz += integralFBz;
+  } else {
+    integralFBx = 0.0f;
+    integralFBy = 0.0f;
+    integralFBz = 0.0f;
+  }
+
+  gx += twoKp * halfex;
+  gy += twoKp * halfey;
+  gz += twoKp * halfez;
+
+  gx *= (0.5f * dt);
+  gy *= (0.5f * dt);
+  gz *= (0.5f * dt);
+
+  float qa = q0, qb = q1, qc = q2;
+  q0 += (-qb * gx - qc * gy - q3 * gz);
+  q1 += (qa * gx + qc * gz - q3 * gy);
+  q2 += (qa * gy - qb * gz + q3 * gx);
+  q3 += (qa * gz + qb * gy - qc * gx);
+
+  recipNorm = 1.0f / sqrtf(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+  q0 *= recipNorm;
+  q1 *= recipNorm;
+  q2 *= recipNorm;
+  q3 *= recipNorm;
+}
+
+void clearLogs() {
+  if (SD.exists("data.log")) { SD.remove("data.log"); Serial.println("YAY"); }
+  if (SD.exists("apogee.log")) SD.remove("apogee.log");
+  File f = SD.open("data.log", FILE_WRITE);
+  if (f) {
+    f.println(F("time_ms,alt_ft,vel_fps,ax,ay,az,tilt_roll,tilt_pitch,pred_apogee_ft"));
+    f.close();
+  }
+  File af = SD.open("apogee.log", FILE_WRITE);
+  if (af) { af.close(); }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  delay(1000);
+  airbrake.attach(SERVO_PIN);
+  airbrake.write(0);
+
+  if (!SD.begin(SD_CS_PIN)) {
+    airbrake.write(120);
+    while (1);
+  }
+  clearLogs();
+
+  if (!bmp.begin_I2C(0x77)) {
+    Serial.println(F("BROO WHY"));
+    airbrake.write(120);
+    while (1);
+  }
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_2X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+
+  mpu.begin();
+
+  for (int i = 0; i < 20; i++) {
+    bmp.performReading();
+    delay(10);
+  }
+  bmp.performReading();
+  groundAlt = bmp.readAltitude(GROUND_PRESSURE_HPA);
+
+  last_time = millis();
+  Serial.println(F("Setup done."));
+}
+
+void loop() {
+  unsigned long t = millis();
+  if (t - last_time < LOOP_DELAY_MS) return;
+  float dt = (t - last_time) / 1000.0f;
+  last_time = t;
+
+  mpu.update();
+
+  bmp.performReading();
+  float alt = bmp.readAltitude(GROUND_PRESSURE_HPA) - groundAlt;
+  altitude_m = kalmanUpdate(alt, est_alt, P);
+  velocity_mps = (altitude_m - prev_alt) / dt;
+  acceleration_mps2 = (velocity_mps - est_vel) / dt;
+  est_vel = velocity_mps;
+  prev_alt = altitude_m;
+
+  float ax_raw = mpu.getAccX();
+  float ay_raw = mpu.getAccY();
+  float az_raw = mpu.getAccZ();
+  float gx_raw = mpu.getGyroX();
+  float gy_raw = mpu.getGyroY();
+  float gz_raw = mpu.getGyroZ();
+
+  float ax = ax_raw;        
+  float ay = az_raw;        
+  float az = -ay_raw;       
+  float gx = gx_raw;        
+  float gy = gz_raw;        
+  float gz = -gy_raw;       
+
+  accel[0] = ax * 9.81f;
+  accel[1] = ay * 9.81f;
+  accel[2] = az * 9.81f;
+
+  MahonyAHRSupdateIMU(gx, gy, gz, ax, ay, az, dt);
+
+  tilt[0] = atan2f(2.0f * (q0 * q1 + q2 * q3),
+                   1 - 2.0f * (q1 * q1 + q2 * q2)) * 57.29578f;
+  tilt[1] = asinf(2.0f * (q0 * q2 - q3 * q1)) * 57.29578f;
+
+  float pitch_rad = radians(tilt[1]);
+  float vertical_velocity = velocity_mps * cos(pitch_rad);
+
+  float predicted_apogee_m = altitude_m +
+                              (vertical_velocity * vertical_velocity) /
+                              (2 * fabs(acceleration_mps2) + 0.01f);
+
+  float predicted_apogee_ft = predicted_apogee_m * 3.28084f;
+  float current_alt_ft = altitude_m * 3.28084f;
+  if (altitude_m > max_alt) max_alt = altitude_m;
+
+  if (!deployed && current_alt_ft > DEPLOY_ALT_MIN_FT) {
+    if (predicted_apogee_ft > TRIGGER_APOGEE_FT) above750_count++;
+    else above750_count = 0;
+    if (above750_count >= PERSIST_COUNT) {
+      airbrake.write(120);
+      deployed = true;
+    }
+  }
+  if (deployed && !retracted && predicted_apogee_ft < TRIGGER_APOGEE_FT) {
+    airbrake.write(0);
+    retracted = true;
+  }
+
+  File f = SD.open("data.log", FILE_WRITE);
+  if (f) {
+    f.print(t); f.print(',');
+    f.print(current_alt_ft, 1); f.print(',');
+    f.print(vertical_velocity * 3.28084f, 1); f.print(',');
+    f.print(accel[0], 2); f.print(',');
+    f.print(accel[1], 2); f.print(',');
+    f.print(accel[2], 2); f.print(',');
+    f.print(tilt[0], 1); f.print(',');
+    f.print(tilt[1], 1); f.print(',');
+    f.println(predicted_apogee_ft, 1);
+    f.close();
+  }
+
+  if (!apogee_logged && est_vel < 0 && acceleration_mps2 < -0.2f) {
+    File af = SD.open("apogee.log", FILE_WRITE);
+    if (af) {
+      af.print(F("Apogee(ft): "));
+      af.println(max_alt * 3.28084f, 1);
+      af.close();
+    }
+    apogee_logged = true;
+  }
+}
